21ebede690cfeb17fec53698c65068cfb40bf56c
Rachit1299 <rachits@thirdrocktechkno.com>
2021-12-21 13:32:38 +0530

    added testing API

---
+++ pages/api/generaterandomkey.js
@@ -0,0 +1,14 @@
+export default async function generaterandomkey(req, res) {
+  try {
+    console.log(req.headers.host);
+    if (req.headers.host === "traek.vercel.app") {
+      console.log("inside IF");
+      res.setHeader("Access-Control-Allow-Origin", req.headers.host);
+      res.contentType = "application/json";
+    }
+    res.status(200);
+  } catch (error) {
+    console.error("GENERATE RANDOMKEY API ERROR: ", error);
+    res.status(500);
+  }
+}
