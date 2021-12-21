eb1c214dddd8fb5de4983ac1b932e14480fdac1e
Rachit1299 <rachits@thirdrocktechkno.com>
2021-12-21 13:53:17 +0530

    testing

--- pages/api/generaterandomkey.js
+++ pages/api/generaterandomkey.js
@@ -1,11 +1,8 @@
 export default async function generaterandomkey(req, res) {
   try {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.contentType = "application/json";
     console.log(req.headers.host);
-    if (req.headers.host) {
-      console.log("inside IF");
-      res.setHeader("Access-Control-Allow-Origin", "*");
-      res.contentType = "application/json";
-    }
     res.status(200).json({ msg: req.headers.host });
   } catch (error) {
     console.error("GENERATE RANDOMKEY API ERROR: ", error);
